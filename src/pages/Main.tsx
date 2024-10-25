d3099c186044b2ce9e4834f7bf2770e51c12ebd8
Sayem98 <sayemabedin.bd@gmail.com>
2024-10-25 11:38:36 +0600

    UI changes

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -19,51 +19,49 @@ export const Main = () => {
   return (
     <div className="w-full bg-black">
       <img src="/assets/images/banner-png.png" alt="main" className="w-full" />
-      <div className="flex gap-5 flex-col md:flex-row">
+      <div className="flex gap-5 flex-col">
         {/* video */}
         <div className="flex-4">
           <video
             ref={videoRef}
-            className=""
-            src="/assets/images/mainvideo.mp4"
-            muted
+            className=" w-full"
+            src="/assets/images/mainvideo2.mov"
+            controls
             loop
           ></video>
         </div>
         {/* story */}
-        <div className="flex-1 flex flex-col justify-start items-center gap-4 text-[#4cc3a9]  px-5 md:px-0">
-          <h1 className="text-4xl font-bold">Study 152 </h1>
-          <p className="font-bold text-lg md:text-2xl">
-            Alpha-152 is the final stage of the Alpha series of hunan weapons, a
-            group of hyperclones created from Kasumi's DNA by the evil genious
-            Donovan. Having just been born, Alpha-152 is immaculate; she is a
-            blake slate, and precisely because of that, the combat instincts
-            that have been imprinted on her psyche are able to be activated
-            mercilessly without exception.
-          </p>
+        <div className="flex-1 flex flex-col md:flex-row justify-start items-center gap-4 text-[#4cc3a9]  px-5 md:px-32 ">
+          <div className="md:w-[40%]">
+            <h1 className="text-4xl font-bold">Lore </h1>
+            <p className="font-bold text-lg md:text-2xl">
+              In Chinese numerology, 6900 reduces to 6 (smoothness, flow) and
+              152 reduces to 8 (wealth, prosperity), suggesting a path from
+              smooth beginnings leading to prosperity.
+            </p>
+          </div>
           <img
             src="/assets/images/alpha-gif.gif"
             alt="alpha gif"
-            className="w-full"
+            className=""
           />
         </div>
       </div>
       <div className="flex mt-5 flex-col md:flex-row">
         <div className="px-5 text-[#4cc3a9]">
-          <h1 className="text-4xl font-bold">
-            Money and Career Meaning of Angel Number 152{" "}
-          </h1>
+          <h1 className="text-4xl font-bold">Study 152 </h1>
           <p className="font-bold  mt-5 text-lg md:text-2xl">
-            In terms of money and career, the angel number 152 indecates that
-            you are on the brink of major breakthroughs and financial abundance.
-            It is a message from the universe to stay focused on your goals,
-            take risks, and trust your abilities to succeed. This number also
-            signifies the importance of balance between work and personal life.
+            Alpha-152 is the final stage of the Alpha series of human weapons, a
+            group of hyperclones created from Kasumi's DNA by the evil genius
+            Donovan. Having just been born, Alpha-152 is immaculate; she is a
+            blank slate, and precisely because of that, the combat instincts
+            that have been imprinted on her psyche are able to be activated
+            mercilessly without exception.
           </p>
           <img
-            src="/assets/images/trump-gif.gif"
+            src="/assets/images/eye.gif"
             alt="alpha gif"
-            className="w-full mt-5"
+            className="w-full mt-5 h-[450px] md:h-[800px]"
           />
         </div>
         <img src="/assets/images/trump.png" alt="main" className="w-full" />
